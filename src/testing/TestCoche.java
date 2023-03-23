cef8ec504ac79c09f34c835e9157072054e70068
Paco Florido <franciscoflorido0@gmail.com>
2023-03-23 23:30:43 +0100

    Creando el archivo TestCoche.java

---
+++ src/testing/TestCoche.java
@@ -0,0 +1,13 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCoche {
+
+	@Test
+	public void test_crear_coche() {
+		
+	}
+}
