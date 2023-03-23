7421ac5239bb63f8ec714d28901e23842b138547
Paco Florido <franciscoflorido0@gmail.com>
2023-03-23 23:49:21 +0100

    Creando el atributo velocidad y haciendo cambios en TestCoche

---
+++ src/testing/Coche.java
@@ -0,0 +1,6 @@
+package testing;
+
+public class Coche {
+
+	public int velocidad = 0;
+}
--- src/testing/TestCoche.java
+++ src/testing/TestCoche.java
@@ -2,12 +2,14 @@ package testing;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestCoche {
 
 	@Test
-	public void test_crear_coche() {
-		
+	public void test_al_crear_coche_su_velocidad_es_cero() {
+		Coche nuevoCoche = new Coche();
+		Assertions.assertEquals(0, nuevoCoche.velocidad);
 	}
 }
